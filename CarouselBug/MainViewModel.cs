1e22619a2643463aceeb0cba5f010e2cd003d51b
bcaceiro <bcaceiro@ipn.pt>
2024-10-11 05:57:26 +0100

    New repro for CarouselView bug in Loop=False

---
+++ CarouselBug/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarouselBug
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        private ObservableCollection<string> _items;
+        private string _selectedItem;
+
+        public ObservableCollection<string> Items
+        {
+            get => _items;
+            set
+            {
+                _items = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            // Initialize the collection with some sample data
+            Items = new ObservableCollection<string>
+        {
+            "Item 1",
+            "Item 2",
+            "Item 3",
+            "Item 4"
+        };
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
